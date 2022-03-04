a6ce7bf026fecd68df942f760ed30dfe77e1ec8b
Mark Lechtermann <mlechtermann@dspace.de>
2022-03-04 14:07:54 +0100

    Unused Methods removed

--- src/dscom/TypeLibConverter.cs
+++ src/dscom/TypeLibConverter.cs
@@ -19,37 +19,27 @@ using dSPACE.Runtime.InteropServices.Writer;
 
 namespace dSPACE.Runtime.InteropServices;
 
-[SuppressMessage("Microsoft.Performance", "CA1812:AvoidUninstantiatedInternalClasses", Justification = "Compatibility")]
-[SuppressMessage("Microsoft.Performance", "CA1822:MarkMembersAsStatic", Justification = "Compatibility")]
+[SuppressMessage("Microsoft.Performance", "CA1812:AvoidUninstantiatedInternalClasses", Justification = "Compatibility to the mscorelib TypeLibConverter class")]
+[SuppressMessage("Microsoft.Performance", "CA1822:MarkMembersAsStatic", Justification = "Compatibility to the mscorelib TypeLibConverter class")]
 public class TypeLibConverter
 {
     /// <summary>Converts an assembly to a COM type library.</summary>
     /// <param name="assembly">The assembly to convert.</param>
     /// <param name="strTypeLibName">The file name of the resulting type library.</param>
-    /// <param name="notifySink">The <see cref="T:System.Runtime.InteropServices.ITypeLibExporterNotifySink" /> interface implemented by the caller.</param>
+    /// <param name="notifySink">The <see cref="T:dSPACE.Runtime.InteropServices.ITypeLibExporterNotifySink" /> interface implemented by the caller.</param>
     /// <returns>An object that implements the <see langword="ITypeLib" /> interface.</returns>
-    public object? ConvertAssemblyToTypeLib(Assembly assembly, string strTypeLibName, ITypeLibExporterNotifySink notifySink)
+    public object? ConvertAssemblyToTypeLib(Assembly assembly, string strTypeLibName, ITypeLibExporterNotifySink? notifySink)
     {
         var options = new TypeLibConverterOptions() { Out = strTypeLibName };
         return ConvertAssemblyToTypeLib(assembly, options, notifySink);
     }
 
     /// <summary>Converts an assembly to a COM type library.</summary>
-    /// <param name="options">The options to configure the converter.</param>
-    /// <param name="notifySink">The <see cref="T:System.Runtime.InteropServices.ITypeLibExporterNotifySink" /> interface implemented by the caller.</param>
-    /// <returns>An object that implements the <see langword="ITypeLib" /> interface.</returns>
-    public object? ConvertAssemblyToTypeLib(TypeLibConverterOptions options, ITypeLibExporterNotifySink notifySink)
-    {
-        var assembly = Assembly.LoadFrom(options.Assembly);
-        return ConvertAssemblyToTypeLib(assembly, options, notifySink);
-    }
-
-    /// <summary>Converts an assembly to a COM type library.</summary>
     /// <param name="assembly">The assembly to convert.</param>
-    /// <param name="options">The options to configure the converter.</param>
-    /// <param name="notifySink">The <see cref="T:System.Runtime.InteropServices.ITypeLibExporterNotifySink" /> interface implemented by the caller.</param>
+    /// <param name="options">The <see cref="T:dSPACE.Runtime.InteropServices.TypeLibConverterOptions" /> to configure the converter.</param>
+    /// <param name="notifySink">The <see cref="T:dSPACE.Runtime.InteropServices.ITypeLibExporterNotifySink" /> interface implemented by the caller.</param>
     /// <returns>An object that implements the <see langword="ITypeLib" /> interface.</returns>
-    public object? ConvertAssemblyToTypeLib(Assembly assembly, TypeLibConverterOptions options, ITypeLibExporterNotifySink notifySink)
+    public object? ConvertAssemblyToTypeLib(Assembly assembly, TypeLibConverterOptions options, ITypeLibExporterNotifySink? notifySink)
     {
         OleAut32.CreateTypeLib2(SYSKIND.SYS_WIN64, options.Out!, out var typelib).ThrowIfFailed("Failed to create type library.");
 
