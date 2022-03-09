c8bc764a644d3582d86dd8e73e3f62517d905bb9
Mark Lechtermann <mark.lechtermann@gmail.com>
2022-03-09 10:48:08 +0100

    Coverage script changed

--- scripts/test.coverage.bat
+++ scripts/test.coverage.bat
@@ -7,7 +7,5 @@ IF NOT EXIST %TEST_RESULTS% (
     mkdir %TEST_RESULTS%
 )
 
-dotnet tool install -g dotnet-reportgenerator-globaltool
-
 dotnet test -f net6.0 -c Release /p:CollectCoverage=true /p:CoverletOutputFormat=lcov /p:CoverletOutput=%TEST_RESULTS%\lcov.info --logger "trx;LogFileName=%TEST_RESULTS%\results.xml" %PWD%..\dscom.sln 
 COPY %TEST_RESULTS%\lcov.net6.0.info %TEST_RESULTS%\lcov.info
