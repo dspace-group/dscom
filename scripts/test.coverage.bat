8a1413e64098c9c98a1e77777291ddd324620c23
Mark Lechtermann <mark.lechtermann@gmail.com>
2022-03-09 08:54:00 +0100

    Split coverage report into seperate scripts.

--- scripts/test.coverage.bat
+++ scripts/test.coverage.bat
@@ -11,6 +11,3 @@ dotnet tool install -g dotnet-reportgenerator-globaltool
 
 dotnet test -f net6.0 -c Release /p:CollectCoverage=true /p:CoverletOutputFormat=lcov /p:CoverletOutput=%TEST_RESULTS%\lcov.info --logger "trx;LogFileName=%TEST_RESULTS%\results.xml" %PWD%..\dscom.sln 
 COPY %TEST_RESULTS%\lcov.net6.0.info %TEST_RESULTS%\lcov.info
-
-dotnet test  -f net6.0 -c Release /p:CollectCoverage=true /p:CoverletOutputFormat=cobertura /p:CoverletOutput="%TEST_RESULTS%\\"  %PWD%..\dscom.sln 
-reportgenerator -reports:"%TEST_RESULTS%\coverage.net6.0.cobertura.xml" -targetdir:"%TEST_RESULTS%\report" -reporttypes:Html
