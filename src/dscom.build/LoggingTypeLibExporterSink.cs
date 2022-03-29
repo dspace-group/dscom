4b092586e891fdd4e89bbcc571ed62511a4b8deb
Carsten Igel <1760987+carstencodes@users.noreply.github.com>
2022-03-29 18:08:05 +0200

    feat: [Logging] Added verbose/detailed logging for TLB export

---
+++ src/dscom.build/LoggingTypeLibExporterSink.cs
@@ -0,0 +1,49 @@
+// Copyright 2022 dSPACE GmbH, Carsten Igel and Contributors
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+using dSPACE.Runtime.InteropServices;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace dSPACE.Build.Tasks.dscom;
+
+internal sealed class LoggingTypeLibExporterSink : ITypeLibExporterNotifySink
+{
+    private readonly TaskLoggingHelper _log;
+
+    internal LoggingTypeLibExporterSink(TaskLoggingHelper log)
+    {
+        this._log = log;
+    }
+
+    void ITypeLibExporterNotifySink.ReportEvent(ExporterEventKind eventKind, int eventCode, string eventMsg)
+    {
+        var importance = eventKind switch
+        {
+            ExporterEventKind.NOTIF_TYPECONVERTED => MessageImportance.Low,
+            ExporterEventKind.NOTIF_CONVERTWARNING => MessageImportance.Normal,
+            ExporterEventKind.ERROR_REFTOINVALIDASSEMBLY => MessageImportance.High,
+            _ => MessageImportance.High,
+        };
+
+        this._log.LogMessage(importance, "Received {0} event. Event Code is {1}: {2}", Enum.GetName(typeof(ExporterEventKind), eventKind), eventCode, eventMsg);
+    }
+
+    object? ITypeLibExporterNotifySink.ResolveRef(Assembly assembly)
+    {
+        return default;
+    }
+}
