4ce9c60e6ffa554baa9a483ba3fca9291591ced7
Carsten Igel <1760987+carstencodes@users.noreply.github.com>
2022-03-30 15:37:09 +0200

    feat: Added documentation for task

--- src/dscom.build/TlbExport.cs
+++ src/dscom.build/TlbExport.cs
@@ -1,4 +1,4 @@
-﻿// Copyright 2022 dSPACE GmbH, Carsten Igel and Contributors
+// Copyright 2022 dSPACE GmbH, Carsten Igel and Contributors
 // 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -21,33 +21,71 @@ namespace dSPACE.Build.Tasks.dscom;
 
 public sealed class TlbExport : Task
 {
+    /// <summary>
+    /// The build context applied to this instance.
+    /// </summary>
     private readonly IBuildContext _context;
 
+    /// <summary>
+    /// Creates a new instance of the <see cref="TlbExport" />
+    /// export class using the specified build context.
+    /// </summary>
+    /// <param name="context">The build context to apply.</param>
     public TlbExport(IBuildContext context)
     {
         _context = context;
     }
 
+    /// <summary>
+    /// Creates a new instance of the <see cref="TlbExport" />
+    /// class using the default build context.
+    /// </summary>
     public TlbExport() : this(new DefaultBuildContext())
     {
     }
 
+    /// <summary>
+    /// Gets or sets the COM Type Library unique id. If this value 
+    /// will be equal to <see cref="Guid.Empty" />, the id of the 
+    /// resulting COM type library will be not be changed.
+    /// </summary>
     public Guid TlbOverriddenId { get; set; } = Guid.Empty;
 
+    /// <summary>
+    /// Gets or sets the name of the resulting COM type library file (tlb).
+    /// This value must be set for this task to work and cannot be left emptry.
+    /// Existing files will be overridden.
+    /// </summary>
     [Required]
     public string TargetFile { get; set; } = string.Empty;
 
+    /// <summary>
+    /// Gets or sets the name of the incoming managed assembly file (dll or exe).
+    /// This value must be set for this task to work and cannot be left empty.
+    /// The file must exist and be readable.
+    /// </summary>
     [Required]
     public string SourceAssemblyFile { get; set; } = string.Empty;
 
+    /// <summary>
+    /// Gets or sets the additional type library files required for conversion.
+    /// </summary>
     public ITaskItem[] TypeLibraryReferences { get; set; } = Array.Empty<ITaskItem>();
 
+    /// <summary>
+    /// Gets or sets the additional type library search paths required for conversion.
+    /// </summary>
     public ITaskItem[] TypeLibraryReferencePaths { get; set; } = Array.Empty<ITaskItem>();
 
+    /// <summary>
+    /// Gets or sets the additional assemblies that might be required for conversion.
+    /// </summary>
     public ITaskItem[] AssemblyPaths { get; set; } = Array.Empty<ITaskItem>();
 
+    /// <inheritdoc cref="Task.Execute()" />
     public override bool Execute()
     {
+        // Create type library converter settings from task paramters
         var settings = new TypeLibConverterSettings()
         {
             Out = TargetFile,
@@ -58,11 +96,13 @@ public sealed class TlbExport : Task
             ASMPath = ConvertTaskItemToFsPath(AssemblyPaths)
         };
 
+        // Issue a warning, if the type library is about to be overridden.
         if (settings.OverrideTlbId != Guid.Empty)
         {
             Log.LogWarning("The default unique id of the resulting type library will be overridden with the following value: {0}", settings.OverrideTlbId);
         }
 
+        // Perform file system checks.
         var checks = new FileSystemChecks(Log);
 
         var result = true;
@@ -71,14 +111,21 @@ public sealed class TlbExport : Task
         checks.VerifyDirectoriesPresent(settings.TLBRefpath, false, ref result);
         checks.VerifyDirectoriesPresent(settings.ASMPath, false, ref result);
 
+        // run conversion, if result has been successful.
         result = result && _context.ConvertAssemblyToTypeLib(settings, Log);
 
+        // report success or failure.
         return result && !Log.HasLoggedErrors;
     }
 
+    /// <summary>
+    /// Takes the <see cref="ITaskItem.ItemSpec" /> of the specified <paramref name="items" />
+    /// and interprets them as file system entry and hence a file or directory path.
+    /// </summary>
+    /// <param name="items">The items to interpret a file system entries.</param>
+    /// <returns>The converted item-spec values.</returns>
     private static string[] ConvertTaskItemToFsPath(IReadOnlyCollection<ITaskItem> items)
     {
         return items.Where(item => item != null).Select(item => item.ItemSpec).ToArray();
     }
-
 }
