d1bc26bfb3eaf05840af7a55c6ca7eae564d16dd
Matthias Nissen <matthias.nissen@gmx.net>
2022-09-14 11:43:25 +0200

    OutProcServer Net48

---
+++ examples/outproc/server/net48/Greeter.cs
@@ -0,0 +1,19 @@
+using System.Runtime.InteropServices;
+
+namespace net48;
+
+[ComVisible(true)]
+// [ComDefaultInterface(typeof(IGreeter))]
+// [ClassInterface(ClassInterfaceType.None)]
+[Guid("A9BD4ABF-1518-4F3C-B017-6BC45F983FF0")]
+public class Greeter : IGreeter
+{
+    public Greeter()
+    {
+
+    }
+    public string SayHello(string name)
+    {
+        return $"Hello {name}. This is the Net48 OutProc server!";
+    }
+}
