e22dfad35f1bd8bc2f7704242452a1a0efb4f45f
Carsten Igel <CIgel@dspace.de>
2022-12-01 14:31:09 +0100

    qa: Added acceptance test that are automatically executed on pull request

---
+++ examples/outproc/scripts/msbuild-acceptance-test.cmd
@@ -0,0 +1,57 @@
+@ECHO off
+
+SET root=%~dp0\..\..\..\
+
+PUSHD %root%
+
+dotnet clean
+dotnet restore
+dotnet msbuild -nodeReuse:False -t:Build -p:Configuration=Release
+dotnet msbuild -nodeReuse:False -t:Pack -p:Configuration=Release
+
+IF NOT EXIST %root%\_packages MKDIR _packages
+
+XCOPY /Y /I /C /F .\src\dscom\bin\Release\*.nupkg _packages\
+XCOPY /Y /I /C /F .\src\dscom.build\bin\Release\*.nupkg _packages\
+
+POPD
+
+PUSHD %~dp0\..
+
+dotnet msbuild -nodeReuse:False -t:Clean -p:Configuration=Release -p:PerformAcceptanceTest=Runtime
+
+dotnet add server\common\contract.csproj package --prerelease -s %root%\_packages dSPACE.Runtime.InteropServices.BuildTasks
+
+dotnet msbuild -nodeReuse:False -t:Restore -p:Configuration=Release -p:PerformAcceptanceTest=Runtime
+dotnet msbuild -nodeReuse:False -t:Build -p:Configuration=Release -p:PerformAcceptanceTest=Runtime -bl
+SET ERRUNTIME=%ERRORLEVEL%
+
+dotnet msbuild -nodeReuse:False -t:Clean -p:Configuration=Release -p:PerformAcceptanceTest=Runtime
+
+dotnet msbuild -nodeReuse:False -t:Restore -p:Configuration=Release -p:PerformAcceptanceTest=NetStandard
+dotnet msbuild -nodeReuse:False -t:Build -p:Configuration=Release -p:PerformAcceptanceTest=NetStandard -bl
+SET ERSTANDARD=%ERRORLEVEL%
+
+dotnet remove server\common\contract.csproj package dSPACE.Runtime.InteropServices.BuildTasks
+
+POPD
+
+SetLocal EnableDelayedExpansion
+
+SET EXITCODE=0
+
+IF NOT "%ERRUNTIME%" == "0" (
+  SET EXITCODE=1
+  ECHO "Runtime specific acceptance test failed."
+)
+
+IF NOT "%ERSTANDARD%" == "0" (
+  SET EXITCODE=1
+  ECHO ".NET Standard 2.0 specific acceptance test failed."
+)
+
+IF "%EXITCODE%" == "0" (
+  ECHO "Acceptance test completed successfully."
+)
+
+EXIT /B %EXITCODE%
