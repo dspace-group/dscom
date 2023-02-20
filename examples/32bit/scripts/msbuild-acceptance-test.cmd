c42520dd840721891f4eb2ce796755e08f9381d9
Carsten Igel <CIgel@dspace.de>
2023-02-20 13:27:41 +0100

    qa: Fixed acceptance tests locally
    It's enough to clear the global cache
    
    msbuild servers must be shut down explicitely

--- examples/32bit/scripts/msbuild-acceptance-test.cmd
+++ examples/32bit/scripts/msbuild-acceptance-test.cmd
@@ -4,6 +4,8 @@ SET root=%~dp0\..\..\..\
 
 PUSHD %root%
 
+dotnet build-server shutdown
+
 dotnet pack -p:Configuration=Release
 
 IF NOT EXIST %root%\_packages MKDIR _packages
@@ -11,7 +13,9 @@ IF NOT EXIST %root%\_packages MKDIR _packages
 XCOPY /Y /I /C /F .\src\dscom\bin\Release\*.nupkg _packages\
 XCOPY /Y /I /C /F .\src\dscom.build\bin\Release\*.nupkg _packages\
 
-dotnet nuget locals all --clear
+dotnet build-server shutdown
+
+dotnet nuget locals global-packages --clear
 
 POPD
 
@@ -19,25 +23,51 @@ PUSHD %~dp0\..\comtestdotnet
 
 dotnet msbuild -nodeReuse:False -t:Clean -p:Configuration=Release -p:PerformAcceptanceTest=Runtime -p:SkipResolvePackageAssets=true
 
+dotnet build-server shutdown
+
 dotnet msbuild -nodeReuse:False -t:Clean -p:Configuration=Release -p:PerformAcceptanceTest=Runtime -p:SkipResolvePackageAssets=true
+
+dotnet build-server shutdown
 
 dotnet add comtestdotnet.csproj package --prerelease -s %root%\_packages dSPACE.Runtime.InteropServices.BuildTasks
 
 dotnet msbuild -nodeReuse:False -t:Restore -p:Configuration=Release -p:Platform=x64 -p:TargetPlatform=net48 -p:PerformAcceptanceTest=Runtime
+
+dotnet build-server shutdown
+
 dotnet msbuild -nodeReuse:False -t:Build -p:Configuration=Release -p:Platform=x64 -p:TargetPlatform=net48 -p:PerformAcceptanceTest=Runtime -bl
+
 SET ERRUNTIMEX64_NET48=%ERRORLEVEL%
 
+dotnet build-server shutdown
+
 dotnet msbuild -nodeReuse:False -t:Restore -p:Configuration=Release -p:Platform=x64 -p:TargetPlatform=net6.0-windows -p:PerformAcceptanceTest=Runtime
+
+dotnet build-server shutdown
+
 dotnet msbuild -nodeReuse:False -t:Build -p:Configuration=Release -p:Platform=x64 -p:TargetPlatform=net6.0-windows -p:PerformAcceptanceTest=Runtime -bl
+
 SET ERRUNTIMEX64_NET60=%ERRORLEVEL%
 
+dotnet build-server shutdown
+
 dotnet msbuild -nodeReuse:False -t:Restore -p:Configuration=Release -p:Platform=x86 -p:TargetPlatform=net48 -p:PerformAcceptanceTest=Runtime
+
+dotnet build-server shutdown
+
 dotnet msbuild -nodeReuse:False -t:Build -p:Configuration=Release -p:Platform=x86 -p:TargetPlatform=net48 -p:PerformAcceptanceTest=Runtime -bl
 SET ERRUNTIMEX86_NET48=%ERRORLEVEL%
 
+dotnet build-server shutdown
+
 dotnet msbuild -nodeReuse:False -t:Restore -p:Configuration=Release -p:Platform=x86 -p:TargetPlatform=net6.0-windows -p:PerformAcceptanceTest=Runtime
+
+dotnet build-server shutdown
+
 dotnet msbuild -nodeReuse:False -t:Build -p:Configuration=Release -p:Platform=x86 -p:TargetPlatform=net6.0-windows -p:PerformAcceptanceTest=Runtime -bl
 SET ERRUNTIMEX86_NET60=%ERRORLEVEL%
+
+dotnet build-server shutdown
 
 dotnet remove comtestdotnet.csproj package dSPACE.Runtime.InteropServices.BuildTasks
 
--- examples/outproc/scripts/msbuild-acceptance-test.cmd
+++ examples/outproc/scripts/msbuild-acceptance-test.cmd
@@ -1,8 +1,10 @@
-@ECHO off
+@ECHO on
 
 SET root=%~dp0\..\..\..\
 
 PUSHD %root%
+
+dotnet build-server shutdown
 
 dotnet pack -p:Configuration=Release
 
@@ -11,29 +13,52 @@ IF NOT EXIST %root%\_packages MKDIR _packages
 XCOPY /Y /I /C /F .\src\dscom\bin\Release\*.nupkg _packages\
 XCOPY /Y /I /C /F .\src\dscom.build\bin\Release\*.nupkg _packages\
 
-dotnet nuget locals all --clear
+dotnet build-server shutdown
+
+dotnet nuget locals global-packages --clear
 
 POPD
 
 PUSHD %~dp0\..
 
+dotnet build-server shutdown
+
 dotnet msbuild -nodeReuse:False -t:Clean -p:Configuration=Release -p:PerformAcceptanceTest=Runtime -p:SkipResolvePackageAssets=true
 
+dotnet build-server shutdown
+
 dotnet msbuild -nodeReuse:False -t:Restore -p:Configuration=Release -p:PerformAcceptanceTest=Runtime
+
+dotnet build-server shutdown
 
 dotnet add server\common\contract.csproj package --prerelease -s %root%\_packages dSPACE.Runtime.InteropServices.BuildTasks
 
 dotnet msbuild -nodeReuse:False -t:Restore -p:Configuration=Release -p:PerformAcceptanceTest=Runtime
+
+dotnet build-server shutdown
+
 dotnet msbuild -nodeReuse:False -t:Build -p:Configuration=Release -p:PerformAcceptanceTest=Runtime -bl
 SET ERRUNTIME=%ERRORLEVEL%
 
-dotnet msbuild -nodeReuse:False -t:Clean -p:Configuration=Release -p:PerformAcceptanceTest=Runtime  -p:SkipResolvePackageAssets=true
+dotnet build-server shutdown
+
+dotnet msbuild -nodeReuse:False -t:Clean -p:Configuration=Release -p:PerformAcceptanceTest=NetStandard  -p:SkipResolvePackageAssets=true
+
+dotnet build-server shutdown
 
 dotnet msbuild -nodeReuse:False -t:Restore -p:Configuration=Release -p:PerformAcceptanceTest=NetStandard
+
+dotnet build-server shutdown
+
 dotnet msbuild -nodeReuse:False -t:Build -p:Configuration=Release -p:PerformAcceptanceTest=NetStandard -bl
+
 SET ERSTANDARD=%ERRORLEVEL%
 
+dotnet build-server shutdown
+
 dotnet remove server\common\contract.csproj package dSPACE.Runtime.InteropServices.BuildTasks
+
+dotnet build-server shutdown
 
 POPD
 
