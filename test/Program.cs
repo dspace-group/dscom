1d0995d4cce1f09640387f30d1b340c8f52ec7d8
Mark Lechtermann <mark.lechtermann@gmail.com>
2023-05-08 08:52:47 +0200

    build: debug

---
+++ test/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.CodeAnalysis.Tools.Utilities;
+
+internal class Program
+{
+    private static int Main(string[] args)
+    {
+        TryGetDotNetCliVersion(out string a);
+        Console.WriteLine(a);
+
+        return 0;
+    }
+
+
+    internal static bool TryGetDotNetCliVersion(out string dotnetVersion)
+    {
+        var processInfo = ProcessRunner.CreateProcess("dotnet", "--version", captureOutput: true, displayWindow: false);
+        var versionResult = processInfo.Result.GetAwaiter().GetResult();
+
+        dotnetVersion = versionResult.OutputLines[0].Trim();
+        return true;
+    }
+}
