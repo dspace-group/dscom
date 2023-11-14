47c8c253382939c6942f368756f8b9e084d024b3
Mitch Gaffigan <mitch.gaffigan@comcast.net>
2023-11-13 22:23:16 -0600

    Add sample library to reproduce export despite missing references

---
+++ src/dscom.demo/assembly5/ExplodeyBaseClass.cs
@@ -0,0 +1,7 @@
+using System.Windows.Controls;
+
+namespace dSPACE.Runtime.InteropServices.DemoAssembly5;
+
+public class ExplodeyBaseClass : UserControl
+{
+}
---
+++ src/dscom.demo/assembly5/IExportableType.cs
@@ -0,0 +1,9 @@
+using System.Runtime.InteropServices;
+
+namespace dSPACE.Runtime.InteropServices.DemoAssembly5;
+
+[ComVisible(true), Guid("E27C3BDB-ACEF-44F9-8568-481D44681C04"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+public interface IExportableType
+{
+    void DoIt();
+}
