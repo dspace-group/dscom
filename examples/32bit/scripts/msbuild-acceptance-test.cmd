4968c4d1818469810d127a72996abcb7f8d9128c
Carsten Igel <cigel@dspace.de>
2024-04-26 13:58:56 +0200

    qa: Modify acceptance test
    Will now run for .NET 8
    Will complain, if TLB is not present

--- examples/32bit/scripts/msbuild-acceptance-test.cmd
+++ examples/32bit/scripts/msbuild-acceptance-test.cmd
@@ -35,7 +35,7 @@ dotnet msbuild -nodeReuse:False -t:Restore -p:Configuration=Release -p:Platform=
 
 dotnet build-server shutdown
 
-dotnet msbuild -nodeReuse:False -t:Build -p:Configuration=Release -p:Platform=x64 -p:TargetPlatform=net48 -p:PerformAcceptanceTest=Runtime -bl
+dotnet msbuild -nodeReuse:False -t:Build -p:Configuration=Release -p:Platform=x64 -p:TargetPlatform=net48 -p:PerformAcceptanceTest=Runtime -bl:%~dp0\net48x64.binlog
 
 SET ERRUNTIMEX64_NET48=%ERRORLEVEL%
 
@@ -45,9 +45,19 @@ dotnet msbuild -nodeReuse:False -t:Restore -p:Configuration=Release -p:Platform=
 
 dotnet build-server shutdown
 
-dotnet msbuild -nodeReuse:False -t:Build -p:Configuration=Release -p:Platform=x64 -p:TargetPlatform=net6.0-windows -p:PerformAcceptanceTest=Runtime -bl
+dotnet msbuild -nodeReuse:False -t:Build -p:Configuration=Release -p:Platform=x64 -p:TargetPlatform=net6.0-windows -p:PerformAcceptanceTest=Runtime -bl:%~dp0\net60x64.binlog
 
 SET ERRUNTIMEX64_NET60=%ERRORLEVEL%
+
+dotnet build-server shutdown
+
+dotnet msbuild -nodeReuse:False -t:Restore -p:Configuration=Release -p:Platform=x64 -p:TargetPlatform=net8.0-windows -p:PerformAcceptanceTest=Runtime
+
+dotnet build-server shutdown
+
+dotnet msbuild -nodeReuse:False -t:Build -p:Configuration=Release -p:Platform=x64 -p:TargetPlatform=net8.0-windows -p:PerformAcceptanceTest=Runtime -bl:%~dp0\net80x64.binlog
+
+SET ERRUNTIMEX64_NET80=%ERRORLEVEL%
 
 dotnet build-server shutdown
 
@@ -55,7 +65,7 @@ dotnet msbuild -nodeReuse:False -t:Restore -p:Configuration=Release -p:Platform=
 
 dotnet build-server shutdown
 
-dotnet msbuild -nodeReuse:False -t:Build -p:Configuration=Release -p:Platform=x86 -p:TargetPlatform=net48 -p:PerformAcceptanceTest=Runtime -bl
+dotnet msbuild -nodeReuse:False -t:Build -p:Configuration=Release -p:Platform=x86 -p:TargetPlatform=net48 -p:PerformAcceptanceTest=Runtime -bl:%~dp0\net48x86.binlog
 SET ERRUNTIMEX86_NET48=%ERRORLEVEL%
 
 dotnet build-server shutdown
@@ -64,8 +74,17 @@ dotnet msbuild -nodeReuse:False -t:Restore -p:Configuration=Release -p:Platform=
 
 dotnet build-server shutdown
 
-dotnet msbuild -nodeReuse:False -t:Build -p:Configuration=Release -p:Platform=x86 -p:TargetPlatform=net6.0-windows -p:PerformAcceptanceTest=Runtime -bl
+dotnet msbuild -nodeReuse:False -t:Build -p:Configuration=Release -p:Platform=x86 -p:TargetPlatform=net6.0-windows -p:PerformAcceptanceTest=Runtime -bl:%~dp0\net60x86.binlog
 SET ERRUNTIMEX86_NET60=%ERRORLEVEL%
+
+dotnet build-server shutdown
+
+dotnet msbuild -nodeReuse:False -t:Restore -p:Configuration=Release -p:Platform=x86 -p:TargetPlatform=net8.0-windows -p:PerformAcceptanceTest=Runtime
+
+dotnet build-server shutdown
+
+dotnet msbuild -nodeReuse:False -t:Build -p:Configuration=Release -p:Platform=x86 -p:TargetPlatform=net8.0-windows -p:PerformAcceptanceTest=Runtime -bl:%~dp0\net80x86.binlog
+SET ERRUNTIMEX86_NET80=%ERRORLEVEL%
 
 dotnet build-server shutdown
 
@@ -79,22 +98,62 @@ SET EXITCODE=0
 
 IF NOT "%ERRUNTIMEX64_NET48%" == "0" (
   SET EXITCODE=1
-  ECHO "Runtime specific acceptance test for platform x64 using .NET FullFramework 4.8 failed."
+  ECHO "Runtime specific acceptance test for platform x64 using .NET FullFramework 4.8 failed." >&2
 )
 
 IF NOT "%ERRUNTIMEX64_NET60%" == "0" (
   SET EXITCODE=1
-  ECHO "Runtime specific acceptance test for platform x64 using .NET 6.0 failed."
+  ECHO "Runtime specific acceptance test for platform x64 using .NET 6.0 failed." >&2
+)
+
+IF NOT "%ERRUNTIMEX64_NET80%" == "0" (
+  SET EXITCODE=1
+  ECHO "Runtime specific acceptance test for platform x64 using .NET 8.0 failed." >&2
 )
 
 IF NOT "%ERRUNTIMEX86_NET48%" == "0" (
   SET EXITCODE=1
-  ECHO "Runtime specific acceptance test for platform x86 using .NET FullFramework 4.8 failed."
+  ECHO "Runtime specific acceptance test for platform x86 using .NET FullFramework 4.8 failed." >&2
+)
+
+IF NOT "%ERRUNTIMEX86_NET80%" == "0" (
+  SET EXITCODE=1
+  ECHO "Runtime specific acceptance test for platform x86 using .NET 8.0 failed." >&2
 )
 
 IF NOT "%ERRUNTIMEX86_NET60%" == "0" (
   SET EXITCODE=1
-  ECHO "Runtime specific acceptance test for platform x64 using .NET 6.0 failed."
+  ECHO "Runtime specific acceptance test for platform x64 using .NET 6.0 failed." >&2
+)
+
+IF NOT EXIST %~dp0\..\comtestdotnet\bin\x64\Release\net6.0\comtestdotnet.tlb (
+  SET EXITCODE=1
+  ECHO "Could not find exported TLB file for .NET 6 (x64)" >&2
+)
+
+IF NOT EXIST %~dp0\..\comtestdotnet\bin\x86\Release\net6.0\comtestdotnet.tlb (
+  SET EXITCODE=1
+  ECHO "Could not find exported TLB file for .NET 6 (x86)" >&2
+)
+
+IF NOT EXIST %~dp0\..\comtestdotnet\bin\x64\Release\net48\comtestdotnet.tlb (
+  SET EXITCODE=1
+  ECHO "Could not find exported TLB file for .NET 4.8 (x64)" >&2
+)
+
+IF NOT EXIST %~dp0\..\comtestdotnet\bin\x86\Release\net48\comtestdotnet.tlb (
+  SET EXITCODE=1
+  ECHO "Could not find exported TLB file for .NET 4.8 (x86)" >&2
+)
+
+IF NOT EXIST %~dp0\..\comtestdotnet\bin\x64\Release\net8.0\comtestdotnet.tlb (
+  SET EXITCODE=1
+  ECHO "Could not find exported TLB file for .NET 8 (x64)" >&2
+)
+
+IF NOT EXIST %~dp0\..\comtestdotnet\bin\x86\Release\net8.0\comtestdotnet.tlb (
+  SET EXITCODE=1
+  ECHO "Could not find exported TLB file for .NET 8 (x86)" >&2
 )
 
 IF "%EXITCODE%" == "0" (
