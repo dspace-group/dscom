f3002196b7778ceeae179fa7e286913f9d2e6ea8
Carsten Igel <cigel@dspace.de>
2024-04-30 16:12:55 +0200

    qa: Using warnings for gh action visibility

--- examples/32bit/scripts/msbuild-acceptance-test.cmd
+++ examples/32bit/scripts/msbuild-acceptance-test.cmd
@@ -98,62 +98,62 @@ SET EXITCODE=0
 
 IF NOT "%ERRUNTIMEX64_NET48%" == "0" (
   SET EXITCODE=1
-  ECHO "Runtime specific acceptance test for platform x64 using .NET FullFramework 4.8 failed." >&2
+  ECHO "::warning file=%~dp0\..\comtestdotnet\comtestdotnet.csproj::Runtime specific acceptance test for platform x64 using .NET FullFramework 4.8 failed."
 )
 
 IF NOT "%ERRUNTIMEX64_NET60%" == "0" (
   SET EXITCODE=1
-  ECHO "Runtime specific acceptance test for platform x64 using .NET 6.0 failed." >&2
+  ECHO "::warning file=%~dp0\..\comtestdotnet\comtestdotnet.csproj::Runtime specific acceptance test for platform x64 using .NET 6.0 failed."
 )
 
 IF NOT "%ERRUNTIMEX64_NET80%" == "0" (
   SET EXITCODE=1
-  ECHO "Runtime specific acceptance test for platform x64 using .NET 8.0 failed." >&2
+  ECHO "::warning file=%~dp0\..\comtestdotnet\comtestdotnet.csproj::Runtime specific acceptance test for platform x64 using .NET 8.0 failed."
 )
 
 IF NOT "%ERRUNTIMEX86_NET48%" == "0" (
   SET EXITCODE=1
-  ECHO "Runtime specific acceptance test for platform x86 using .NET FullFramework 4.8 failed." >&2
+  ECHO "::warning file=%~dp0\..\comtestdotnet\comtestdotnet.csproj::Runtime specific acceptance test for platform x86 using .NET FullFramework 4.8 failed."
 )
 
 IF NOT "%ERRUNTIMEX86_NET80%" == "0" (
   SET EXITCODE=1
-  ECHO "Runtime specific acceptance test for platform x86 using .NET 8.0 failed." >&2
+  ECHO "::warning file=%~dp0\..\comtestdotnet\comtestdotnet.csproj::Runtime specific acceptance test for platform x86 using .NET 8.0 failed."
 )
 
 IF NOT "%ERRUNTIMEX86_NET60%" == "0" (
   SET EXITCODE=1
-  ECHO "Runtime specific acceptance test for platform x64 using .NET 6.0 failed." >&2
+  ECHO "::warning file=%~dp0\..\comtestdotnet\bin\x64\Release\net6.0\comtestdotnet.tlb::Runtime specific acceptance test for platform x64 using .NET 6.0 failed."
 )
 
 IF NOT EXIST %~dp0\..\comtestdotnet\bin\x64\Release\net6.0\comtestdotnet.tlb (
   SET EXITCODE=1
-  ECHO "Could not find exported TLB file for .NET 6 (x64)" >&2
+  ECHO "::warning file=%~dp0\..\comtestdotnet\bin\x64\Release\net6.0\comtestdotnet.tlb::Could not find exported TLB file for .NET 6 (x64)"
 )
 
 IF NOT EXIST %~dp0\..\comtestdotnet\bin\x86\Release\net6.0\comtestdotnet.tlb (
   SET EXITCODE=1
-  ECHO "Could not find exported TLB file for .NET 6 (x86)" >&2
+  ECHO "::warning file=%~dp0\..\comtestdotnet\bin\x86\Release\net6.0\comtestdotnet.tlb::Could not find exported TLB file for .NET 6 (x86)"
 )
 
 IF NOT EXIST %~dp0\..\comtestdotnet\bin\x64\Release\net48\comtestdotnet.tlb (
   SET EXITCODE=1
-  ECHO "Could not find exported TLB file for .NET 4.8 (x64)" >&2
+  ECHO "::warning file=%~dp0\..\comtestdotnet\bin\x64\Release\net48\comtestdotnet.tlb::Could not find exported TLB file for .NET 4.8 (x64)"
 )
 
 IF NOT EXIST %~dp0\..\comtestdotnet\bin\x86\Release\net48\comtestdotnet.tlb (
   SET EXITCODE=1
-  ECHO "Could not find exported TLB file for .NET 4.8 (x86)" >&2
+  ECHO "::warning file=%~dp0\..\comtestdotnet\bin\x86\Release\net48\comtestdotnet.tlb::Could not find exported TLB file for .NET 4.8 (x86)"
 )
 
 IF NOT EXIST %~dp0\..\comtestdotnet\bin\x64\Release\net8.0\comtestdotnet.tlb (
   SET EXITCODE=1
-  ECHO "Could not find exported TLB file for .NET 8 (x64)" >&2
+  ECHO "::warning file=%~dp0\..\comtestdotnet\bin\x64\Release\net8.0\comtestdotnet.tlb::Could not find exported TLB file for .NET 8 (x64)"
 )
 
 IF NOT EXIST %~dp0\..\comtestdotnet\bin\x86\Release\net8.0\comtestdotnet.tlb (
   SET EXITCODE=1
-  ECHO "Could not find exported TLB file for .NET 8 (x86)" >&2
+  ECHO "::warning file=%~dp0\..\comtestdotnet\bin\x86\Release\net8.0\comtestdotnet.tlb::Could not find exported TLB file for .NET 8 (x86)"
 )
 
 IF "%EXITCODE%" == "0" (
