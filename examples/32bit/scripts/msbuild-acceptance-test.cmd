ab38e8582439ac68e1aef03391d4e5407050c7be
Carsten Igel <1760987+carstencodes@users.noreply.github.com>
2024-04-30 16:17:47 +0200

    fix: x86 compatibility of build task (#248)
    * fix: Call BuildTarget for all platforms
    
    Closes #247
    
    * qa: Modify acceptance test
    
    Will now run for .NET 8
    Will complain, if TLB is not present
    
    ---------
    
    Co-authored-by: Carsten Igel <cigel@dspace.de>

--- examples/32bit/scripts/msbuild-acceptance-test.cmd
+++ examples/32bit/scripts/msbuild-acceptance-test.cmd
@@ -6,7 +6,7 @@ PUSHD %root%
 
 dotnet build-server shutdown
 
-dotnet pack -p:Configuration=Release
+dotnet pack src\dscom.build\dscom.build.csproj -p:Configuration=Release
 
 IF NOT EXIST %root%\_packages MKDIR _packages
 
@@ -35,7 +35,7 @@ dotnet msbuild -nodeReuse:False -t:Restore -p:Configuration=Release -p:Platform=
 
 dotnet build-server shutdown
 
-dotnet msbuild -nodeReuse:False -t:Build -p:Configuration=Release -p:Platform=x64 -p:TargetPlatform=net48 -p:PerformAcceptanceTest=Runtime -bl
+dotnet msbuild -nodeReuse:False -t:Build -p:Configuration=Release -p:Platform=x64 -p:TargetPlatform=net48 -p:PerformAcceptanceTest=Runtime -bl:%~dp0\net48x64.binlog
 
 SET ERRUNTIMEX64_NET48=%ERRORLEVEL%
 
@@ -45,9 +45,19 @@ dotnet msbuild -nodeReuse:False -t:Restore -p:Configuration=Release -p:Platform=
 
 dotnet build-server shutdown
 
-dotnet msbuild -nodeReuse:False -t:Build -p:Configuration=Release -p:Platform=x64 -p:TargetPlatform=net6.0-windows -p:PerformAcceptanceTest=Runtime -bl
+dotnet msbuild -nodeReuse:False -t:Build -p:Configuration=Release -p:Platform=x64 -p:TargetPlatform=net6.0-windows -p:PerformAcceptanceTest=Runtime -bl:%~dp0\net60x64.binlog
 
 SET ERRUNTIMEX64_NET60=%ERRORLEVEL%
+
+dotnet build-server shutdown
+
+dotnet msbuild -nodeReuse:False -t:Restore -p:Configuration=Release -p:Platform=x64 -p:TargetPlatform=net8.0-windows -p:PerformAcceptanceTest=Runtime
+
+dotnet build-server shutdown
+
+dotnet msbuild -nodeReuse:False -t:Build -p:Configuration=Release -p:Platform=x64 -p:TargetPlatform=net8.0-windows -p:PerformAcceptanceTest=Runtime -bl:%~dp0\net80x64.binlog
+
+SET ERRUNTIMEX64_NET80=%ERRORLEVEL%
 
 dotnet build-server shutdown
 
@@ -55,7 +65,7 @@ dotnet msbuild -nodeReuse:False -t:Restore -p:Configuration=Release -p:Platform=
 
 dotnet build-server shutdown
 
-dotnet msbuild -nodeReuse:False -t:Build -p:Configuration=Release -p:Platform=x86 -p:TargetPlatform=net48 -p:PerformAcceptanceTest=Runtime -bl
+dotnet msbuild -nodeReuse:False -t:Build -p:Configuration=Release -p:Platform=x86 -p:TargetPlatform=net48 -p:PerformAcceptanceTest=Runtime -bl:%~dp0\net48x86.binlog
 SET ERRUNTIMEX86_NET48=%ERRORLEVEL%
 
 dotnet build-server shutdown
@@ -64,8 +74,17 @@ dotnet msbuild -nodeReuse:False -t:Restore -p:Configuration=Release -p:Platform=
 
 dotnet build-server shutdown
 
-dotnet msbuild -nodeReuse:False -t:Build -p:Configuration=Release -p:Platform=x86 -p:TargetPlatform=net6.0-windows -p:PerformAcceptanceTest=Runtime -bl
+dotnet msbuild -nodeReuse:False -t:Build -p:Configuration=Release -p:Platform=x86 -p:TargetPlatform=net6.0-windows -p:PerformAcceptanceTest=Runtime -bl:%~dp0\net60x86.binlog
 SET ERRUNTIMEX86_NET60=%ERRORLEVEL%
+
+dotnet build-server shutdown
+
+dotnet msbuild -nodeReuse:False -t:Restore -p:Configuration=Release -p:Platform=x86 -p:TargetPlatform=net8.0-windows -p:PerformAcceptanceTest=Runtime
+
+dotnet build-server shutdown
+
+dotnet msbuild -nodeReuse:False -t:Build -p:Configuration=Release -p:Platform=x86 -p:TargetPlatform=net8.0-windows -p:PerformAcceptanceTest=Runtime -bl:%~dp0\net80x86.binlog
+SET ERRUNTIMEX86_NET80=%ERRORLEVEL%
 
 dotnet build-server shutdown
 
@@ -79,22 +98,62 @@ SET EXITCODE=0
 
 IF NOT "%ERRUNTIMEX64_NET48%" == "0" (
   SET EXITCODE=1
-  ECHO "Runtime specific acceptance test for platform x64 using .NET FullFramework 4.8 failed."
+  ECHO "::warning file=%~dp0\..\comtestdotnet\comtestdotnet.csproj::Runtime specific acceptance test for platform x64 using .NET FullFramework 4.8 failed."
 )
 
 IF NOT "%ERRUNTIMEX64_NET60%" == "0" (
   SET EXITCODE=1
-  ECHO "Runtime specific acceptance test for platform x64 using .NET 6.0 failed."
+  ECHO "::warning file=%~dp0\..\comtestdotnet\comtestdotnet.csproj::Runtime specific acceptance test for platform x64 using .NET 6.0 failed."
+)
+
+IF NOT "%ERRUNTIMEX64_NET80%" == "0" (
+  SET EXITCODE=1
+  ECHO "::warning file=%~dp0\..\comtestdotnet\comtestdotnet.csproj::Runtime specific acceptance test for platform x64 using .NET 8.0 failed."
 )
 
 IF NOT "%ERRUNTIMEX86_NET48%" == "0" (
-  SET EXITCODE=1
-  ECHO "Runtime specific acceptance test for platform x86 using .NET FullFramework 4.8 failed."
+  ::SET EXITCODE=1
+  ECHO "::warning file=%~dp0\..\comtestdotnet\comtestdotnet.csproj::Runtime specific acceptance test for platform x86 using .NET FullFramework 4.8 failed."
+)
+
+IF NOT "%ERRUNTIMEX86_NET80%" == "0" (
+  ::SET EXITCODE=1
+  ECHO "::warning file=%~dp0\..\comtestdotnet\comtestdotnet.csproj::Runtime specific acceptance test for platform x86 using .NET 8.0 failed."
 )
 
 IF NOT "%ERRUNTIMEX86_NET60%" == "0" (
+  ::SET EXITCODE=1
+  ECHO "::warning file=%~dp0\..\comtestdotnet\bin\x64\Release\net6.0\comtestdotnet.tlb::Runtime specific acceptance test for platform x64 using .NET 6.0 failed."
+)
+
+IF NOT EXIST %~dp0\..\comtestdotnet\bin\x64\Release\net6.0\comtestdotnet.tlb (
   SET EXITCODE=1
-  ECHO "Runtime specific acceptance test for platform x64 using .NET 6.0 failed."
+  ECHO "::warning file=%~dp0\..\comtestdotnet\bin\x64\Release\net6.0\comtestdotnet.tlb::Could not find exported TLB file for .NET 6 (x64)"
+)
+
+IF NOT EXIST %~dp0\..\comtestdotnet\bin\x86\Release\net6.0\comtestdotnet.tlb (
+  ::SET EXITCODE=1
+  ECHO "::warning file=%~dp0\..\comtestdotnet\bin\x86\Release\net6.0\comtestdotnet.tlb::Could not find exported TLB file for .NET 6 (x86)"
+)
+
+IF NOT EXIST %~dp0\..\comtestdotnet\bin\x64\Release\net48\comtestdotnet.tlb (
+  SET EXITCODE=1
+  ECHO "::warning file=%~dp0\..\comtestdotnet\bin\x64\Release\net48\comtestdotnet.tlb::Could not find exported TLB file for .NET 4.8 (x64)"
+)
+
+IF NOT EXIST %~dp0\..\comtestdotnet\bin\x86\Release\net48\comtestdotnet.tlb (
+  ::SET EXITCODE=1
+  ECHO "::warning file=%~dp0\..\comtestdotnet\bin\x86\Release\net48\comtestdotnet.tlb::Could not find exported TLB file for .NET 4.8 (x86)"
+)
+
+IF NOT EXIST %~dp0\..\comtestdotnet\bin\x64\Release\net8.0\comtestdotnet.tlb (
+  SET EXITCODE=1
+  ECHO "::warning file=%~dp0\..\comtestdotnet\bin\x64\Release\net8.0\comtestdotnet.tlb::Could not find exported TLB file for .NET 8 (x64)"
+)
+
+IF NOT EXIST %~dp0\..\comtestdotnet\bin\x86\Release\net8.0\comtestdotnet.tlb (
+  ::SET EXITCODE=1
+  ECHO "::warning file=%~dp0\..\comtestdotnet\bin\x86\Release\net8.0\comtestdotnet.tlb::Could not find exported TLB file for .NET 8 (x86)"
 )
 
 IF "%EXITCODE%" == "0" (
