84d6bfed0c609eb4635067a13f0e9e2d54901a81
Carsten Igel <cigel@dspace.de>
2024-04-30 16:13:24 +0200

    fix: Temporarily ignore failing x86 tests
    As suggested by @marklechtermann

--- examples/32bit/scripts/msbuild-acceptance-test.cmd
+++ examples/32bit/scripts/msbuild-acceptance-test.cmd
@@ -112,17 +112,17 @@ IF NOT "%ERRUNTIMEX64_NET80%" == "0" (
 )
 
 IF NOT "%ERRUNTIMEX86_NET48%" == "0" (
-  SET EXITCODE=1
+  ::SET EXITCODE=1
   ECHO "::warning file=%~dp0\..\comtestdotnet\comtestdotnet.csproj::Runtime specific acceptance test for platform x86 using .NET FullFramework 4.8 failed."
 )
 
 IF NOT "%ERRUNTIMEX86_NET80%" == "0" (
-  SET EXITCODE=1
+  ::SET EXITCODE=1
   ECHO "::warning file=%~dp0\..\comtestdotnet\comtestdotnet.csproj::Runtime specific acceptance test for platform x86 using .NET 8.0 failed."
 )
 
 IF NOT "%ERRUNTIMEX86_NET60%" == "0" (
-  SET EXITCODE=1
+  ::SET EXITCODE=1
   ECHO "::warning file=%~dp0\..\comtestdotnet\bin\x64\Release\net6.0\comtestdotnet.tlb::Runtime specific acceptance test for platform x64 using .NET 6.0 failed."
 )
 
@@ -132,7 +132,7 @@ IF NOT EXIST %~dp0\..\comtestdotnet\bin\x64\Release\net6.0\comtestdotnet.tlb (
 )
 
 IF NOT EXIST %~dp0\..\comtestdotnet\bin\x86\Release\net6.0\comtestdotnet.tlb (
-  SET EXITCODE=1
+  ::SET EXITCODE=1
   ECHO "::warning file=%~dp0\..\comtestdotnet\bin\x86\Release\net6.0\comtestdotnet.tlb::Could not find exported TLB file for .NET 6 (x86)"
 )
 
@@ -142,7 +142,7 @@ IF NOT EXIST %~dp0\..\comtestdotnet\bin\x64\Release\net48\comtestdotnet.tlb (
 )
 
 IF NOT EXIST %~dp0\..\comtestdotnet\bin\x86\Release\net48\comtestdotnet.tlb (
-  SET EXITCODE=1
+  ::SET EXITCODE=1
   ECHO "::warning file=%~dp0\..\comtestdotnet\bin\x86\Release\net48\comtestdotnet.tlb::Could not find exported TLB file for .NET 4.8 (x86)"
 )
 
@@ -152,7 +152,7 @@ IF NOT EXIST %~dp0\..\comtestdotnet\bin\x64\Release\net8.0\comtestdotnet.tlb (
 )
 
 IF NOT EXIST %~dp0\..\comtestdotnet\bin\x86\Release\net8.0\comtestdotnet.tlb (
-  SET EXITCODE=1
+  ::SET EXITCODE=1
   ECHO "::warning file=%~dp0\..\comtestdotnet\bin\x86\Release\net8.0\comtestdotnet.tlb::Could not find exported TLB file for .NET 8 (x86)"
 )
 
