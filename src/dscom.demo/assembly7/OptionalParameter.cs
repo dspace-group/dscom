c25b386e74fe0632f9b6bfc2b94659d172ddacb3
Mark Lechtermann <mark.lechtermann@gmail.com>
2024-09-30 08:54:05 +0200

    chore: Added demo assembly

---
+++ src/dscom.demo/assembly7/OptionalParameter.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel;
+using System.Runtime.InteropServices;
+
+namespace dSPACE.Runtime.InteropServices.DemoAssembly7;
+
+[ComVisible(true)]
+public interface IOptionalParameter
+{
+    int MyProperty1
+    {
+        [Description("MyPropertyDescription1")]
+        get; set;
+    }
+
+    [Description("MyPropertyDescription2")]
+    int MyProperty2 { get; set; }
+}
