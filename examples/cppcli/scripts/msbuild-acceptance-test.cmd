109ec8a246f7a22e5d98a49134059ac64a7ff43a
Carsten Igel <cigel@dspace.de>
2024-11-20 14:47:50 +0100

    qa: Migrate test to x64

--- examples/cppcli/scripts/msbuild-acceptance-test.cmd
+++ examples/cppcli/scripts/msbuild-acceptance-test.cmd
@@ -39,14 +39,14 @@ dotnet add CsLibrary.csproj package --prerelease -s %root%\_packages dSPACE.Runt
 
 dotnet build-server shutdown
 
-msbuild -nodeReuse:False -t:Restore -p:Configuration=Release -p:Platform=x86 -p:TargetPlatform=net8.0-windows -p:PerformAcceptanceTest=Runtime -p:SkipResolvePackageAssets=true
+msbuild -nodeReuse:False -t:Restore -p:Configuration=Release -p:Platform=x64 -p:TargetPlatform=net8.0-windows -p:PerformAcceptanceTest=Runtime -p:SkipResolvePackageAssets=true
 
 dotnet build-server shutdown
 
-msbuild -nodeReuse:False -t:Build -p:Configuration=Release -p:Platform=x86 -p:TargetPlatform=net8.0-windows -p:PerformAcceptanceTest=Runtime -bl:%~dp0\net80x86.binlog
-SET ERRUNTIMEX86_NET80=%ERRORLEVEL%
+msbuild -nodeReuse:False -t:Build -p:Configuration=Release -p:Platform=x64 -p:TargetPlatform=net8.0-windows -p:PerformAcceptanceTest=Runtime -bl:%~dp0\net80x86.binlog
+SET ERRUNTIMEX64_NET80=%ERRORLEVEL%
 
-msbuild -nodeReuse:False -t:Clean -p:Configuration=Release -p:Platform=x86 -p:TargetPlatform=net8.0-windows -p:PerformAcceptanceTest=Runtime ..\CppLibrary\CppLibrary.vcxproj
+msbuild -nodeReuse:False -t:Clean -p:Configuration=Release -p:Platform=x64 -p:TargetPlatform=net8.0-windows -p:PerformAcceptanceTest=Runtime ..\CppLibrary\CppLibrary.vcxproj
 
 dotnet build-server shutdown
 
@@ -58,14 +58,14 @@ SetLocal EnableDelayedExpansion
 
 SET EXITCODE=0
 
-IF NOT "%ERRUNTIMEX86_NET80%" == "0" (
+IF NOT "%ERRUNTIMEX64_NET80%" == "0" (
   SET EXITCODE=1
-  ECHO "::warning::Runtime specific acceptance test for platform x86 using .NET 8.0 failed."
+  ECHO "::warning::Runtime specific acceptance test for platform x64 using .NET 8.0 failed."
 )
 
-IF NOT EXIST %~dp0\..\CsLibrary\bin\x86\Release\net8.0-windows\CsLibrary.tlb (
+IF NOT EXIST %~dp0\..\CsLibrary\bin\x64\Release\net8.0-windows\CsLibrary.tlb (
   ::SET EXITCODE=1
-  ECHO "::warning::Could not find exported TLB file for .NET 8 (x86)"
+  ECHO "::warning::Could not find exported TLB file for .NET 8 (x64)"
 )
 
 IF "%EXITCODE%" == "0" (
