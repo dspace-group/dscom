91e7f28b989028ca032301f2d5369b5d0083cf67
Carsten Igel <cigel@dspace.de>
2024-11-20 09:32:30 +0100

    fix: Update acceptance test to run only on net8.0

--- examples/cppcli/scripts/msbuild-acceptance-test.cmd
+++ examples/cppcli/scripts/msbuild-acceptance-test.cmd
@@ -30,17 +30,6 @@ dotnet add CsLibrary.csproj package --no-restore --prerelease -s %root%\_package
 
 dotnet build-server shutdown
 
-msbuild -nodeReuse:False -t:Restore -p:Configuration=Release -p:Platform=x86 -p:TargetPlatform=net6.0-windows -p:PerformAcceptanceTest=Runtime -p:AcceptanceTestFramework=net6
-
-dotnet build-server shutdown
-
-msbuild -nodeReuse:False -t:Build -p:Configuration=Release -p:Platform=x86 -p:TargetPlatform=net6.0-windows -p:PerformAcceptanceTest=Runtime -p:AcceptanceTestFramework=net6 -bl:%~dp0\net60x86.binlog
-SET ERRUNTIMEX86_NET60=%ERRORLEVEL%
-
-msbuild -nodeReuse:False -t:Clean -p:Configuration=Release -p:Platform=x86 -p:TargetPlatform=net6.0-windows -p:PerformAcceptanceTest=Runtime -p:AcceptanceTestFramework=net6 ..\CppLibrary\CppLibrary.vcxproj
-
-dotnet build-server shutdown
-
 msbuild -nodeReuse:False -t:Restore -p:Configuration=Release -p:Platform=x86 -p:TargetPlatform=net8.0-windows -p:PerformAcceptanceTest=Runtime
 
 dotnet build-server shutdown
@@ -61,22 +50,12 @@ SetLocal EnableDelayedExpansion
 SET EXITCODE=0
 
 IF NOT "%ERRUNTIMEX86_NET80%" == "0" (
-  ::SET EXITCODE=1
+  SET EXITCODE=1
   ECHO "::warning::Runtime specific acceptance test for platform x86 using .NET 8.0 failed."
 )
 
-IF NOT "%ERRUNTIMEX86_NET60%" == "0" (
-  ::SET EXITCODE=1
-  ECHO "::warning::Runtime specific acceptance test for platform x64 using .NET 6.0 failed."
-)
-
-IF NOT EXIST %~dp0\..\CsLibrary\bin\x86\Release\net6.0-windows\CsLibrary.tlb (
-  ::SET EXITCODE=1
-  ECHO "::warning::Could not find exported TLB file for .NET 6 (x86)"
-)
-
 IF NOT EXIST %~dp0\..\CsLibrary\bin\x86\Release\net8.0-windows\CsLibrary.tlb (
-  ::SET EXITCODE=1
+  SET EXITCODE=1
   ECHO "::warning::Could not find exported TLB file for .NET 8 (x86)"
 )
 
