17f751e0d58f542b53f8ce66f1cd1993432ec7fc
Manuel Leßmann <MLessmann@dspace.de>
2025-01-06 15:02:55 +0100

    Try TLBs with matching names first

--- src/dscom/TypeInfoResolver.cs
+++ src/dscom/TypeInfoResolver.cs
@@ -1,11 +1,11 @@
 // Copyright 2022 dSPACE GmbH, Mark Lechtermann, Matthias Nissen and Contributors
-// 
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -116,13 +116,29 @@ internal sealed class TypeInfoResolver : ITypeLibCache
                 var typeLib = GetTypeLibFromIdentifier(identifier);
                 if (typeLib == null)
                 {
-                    foreach (var additionalLib in _additionalLibs)
+                    var name = assembly.GetName().Name ?? string.Empty;
+                    var additionalLibsWithMatchingName = _additionalLibs.Where(additionalLib => additionalLib.Contains(name));
+
+                    // At first we try to find a type library that matches the assembly name.
+                    foreach (var additionalLib in additionalLibsWithMatchingName)
                     {
                         AddTypeLib(additionalLib);
                         retval = ResolveTypeInfo(type.GUID);
-                        if (retval != null)
+                        break;
+                    }
+
+                    // If no type was found in a type library with matching name we search in the remaining type libraries.
+                    if (retval == null)
+                    {
+                        var additionalLibsWithoutMatchingName = _additionalLibs.Except(additionalLibsWithMatchingName);
+                        foreach (var additionalLib in additionalLibsWithoutMatchingName)
                         {
-                            break;
+                            AddTypeLib(additionalLib);
+                            retval = ResolveTypeInfo(type.GUID);
+                            if (retval != null)
+                            {
+                                break;
+                            }
                         }
                     }
 
